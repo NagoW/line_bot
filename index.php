44dd284bbd354fa5e95ec3fb2485079f2b4ed793
NagoW <48646485+NagoW@users.noreply.github.com>
2019-03-19 12:59:24 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
     // Composerでインストールしたライブラリを一括読み込み
-    require_once __DIR__ . 'C:\Users\hmtmk\Line_Bot\gitfile\vendor\autoload.php';
+    require_once __DIR__ . '\vendor\autoload.php';
 
     // POSTメソッドで渡される値を取得、表示
     $inputString = file_get_contents('php://input');
